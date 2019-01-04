d266e6d17ae421905ae864eb70d262093bfd4542
tenzin <ten13zin@gmail.com>
2019-01-05 00:01:41 +0530

    download all the volumes and apply line-seg

--- main.py
+++ main.py
@@ -2,25 +2,51 @@ import argparse
 import os
 import glob
 import subprocess
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 import cv2
 import numpy as np
 
-def page_name(fn, offset):
+def page_length():
+    with open('data/links.html') as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    pg_len = []
+    for a in soup.find_all('a'):
+        o = a['href'].split('%')
+        code = o[1].split('C')[-1]
+        pg = o[2].split('C')[-1]
+        ln = o[-1].split('C')[-1]
+        pg_len.append((code, pg, ln))
+    return pg_len
+
+
+def download_all_volumes():
+    for code, vol, ln in page_length():
+        vol_dir = 'data/volumes/' + 'vol-{}'.format(vol)
+        if not os.path.exists(vol_dir):
+             os.makedirs(vol_dir)
+        print('[INFO]  Volume-{} downloading....'.format(vol))
+        for pg in tqdm(range(3, int(ln)+1)):
+            url = 'https://www.tbrc.org/browser/ImageService?work=W22084&igroup={}&image={}&first=1&last={}&fetchimg=yes'.format(code, pg+2, ln)
+            page_fn = vol_dir + '/page-{0:03d}.png'.format(pg)
+            with open(page_fn, 'wb') as f:
+                f.write(urlopen(url).read())
+            yield page_fn
+
+
+def page_name(fn):
     f_dir = fn.split("/")
     fn = f_dir[-1]
-    vol_dir = os.path.join('output', f_dir[-2])
+    vol_dir = os.path.join('output/volumes', f_dir[-2])
     if not os.path.exists(vol_dir):
             os.makedirs(vol_dir)
-    name, ext = fn.split('.')
-    prefix, idx = name.split('-')
-    idx = int(idx) - offset
-    fn = prefix + '-' + str(idx) + '.' + ext
     fn_path = os.path.join(vol_dir, fn)
     return fn_path
 
 
-def preprocess(fn, offset):
+def preprocess(fn):
     '''Reize the image, set correct page number and set dpi to 300
        Return: correct page number of Pecha'''
 
@@ -74,33 +100,30 @@ def preprocess(fn, offset):
         return resized
 
     img = cv2.imread(fn)
-    fn = page_name(fn, offset)
+    fn = page_name(fn)
 
     img = _resize(img)
-    print(img.shape)
     img = _remove_border(img)
     # img.save(fn, dpi=(300, 300))
     cv2.imwrite(fn, img)
     return fn
 
 def pageseg(fn):
-    cmd = 'python pageseg.py --maxlines 9 --usegauss --maxcolseps 0 ' + fn
+    cmd = 'ocropus_venv/bin/python pageseg.py --maxlines 9 --usegauss --maxcolseps 0 ' + fn
     # subprocess.check_output(cmd)
     subprocess.call(cmd, shell=True)
 
-def process(path, offset):
-    fns = glob.glob(path + "/*")
-    for i, fn in enumerate(fns):
-        fn = preprocess(fn, offset)
-        print(i, fn)
+def process():
+    for fn in download_all_volumes():
+        fn = preprocess(fn)
         pageseg(fn)
 
 
 if __name__ == "__main__":
 
-    ap = argparse.ArgumentParser(add_help=False)
-    ap.add_argument("--dir", type=str, help="path to volume")
-    ap.add_argument("--offset", type=int, help="page offset", default=2)
-    args = ap.parse_args()
+    # ap = argparse.ArgumentParser(add_help=False)
+    # ap.add_argument("--dir", type=str, help="path to volume")
+    # ap.add_argument("--offset", type=int, help="page offset", default=2)
+    # args = ap.parse_args()
 
-    process(args.dir, args.offset)
+    process()
