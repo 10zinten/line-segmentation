7655e73198e034ecd4e78b419cbab4ca990bd019
tenzin <ten13zin@gmail.com>
2019-01-18 14:28:20 +0000

    add: catching the None texts

--- google_orc.py
+++ google_orc.py
@@ -3,20 +3,24 @@ from google.cloud import vision
 from google.cloud.vision import types
 
 vision_client = vision.ImageAnnotatorClient()
-img_fn = 'data/book-097/page-051.png'
 
 def text_annotations(fn):
-    with io.open(img_fn, 'rb') as image_file:
+    with io.open(fn, 'rb') as image_file:
         content = image_file.read()
         image = types.Image(content=content)
 
     response = vision_client.document_text_detection(image=image)
     texts = response.text_annotations
-    
-    return texts[0].description.split('\n')
 
+    try:
+        return texts[0].description
+    except:
+        return ""
+
+    
 if __name__ == "__main__":
 
+    img_fn = 'data/book-097/page-051.png'
     texts = text_annotations(img_fn)
     print(texts)
     print("Number of Lines:", len(texts))
