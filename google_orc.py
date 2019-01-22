281e633efe73085c1f78199bdd0e20a131bd3904
tenzin <ten13zin@gmail.com>
2019-01-22 18:52:08 +0530

    do ocr on all images in given dir

--- google_orc.py
+++ google_orc.py
@@ -1,6 +1,11 @@
+import argparse
 import io
+import glob
+import os
+
 from google.cloud import vision
 from google.cloud.vision import types
+
 
 vision_client = vision.ImageAnnotatorClient()
 
@@ -17,10 +22,25 @@ def text_annotations(fn):
     except:
         return ""
 
-    
+
 if __name__ == "__main__":
 
-    img_fn = 'data/book-097/page-051.png'
-    texts = text_annotations(img_fn)
-    print(texts)
-    print("Number of Lines:", len(texts))
+    from tqdm import tqdm
+
+    ap = argparse.ArgumentParser(add_help=False)
+    ap.add_argument("--dir", type=str, help="url of image")
+    ap.add_argument("--image", type=int, help="start page number", default=1)
+    args = ap.parse_args()
+
+    fns = sorted([o for o in glob.glob(args.dir + '/*') if o.endswith('.png')])
+
+    for fn in tqdm(fns[args.image-1:]):
+        output_dir = os.path.join(args.dir, 'texts')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_fn = fn.split('/')[-1].split('.')[0] + '.txt'
+        output_pg_fn = os.path.join(output_dir, output_fn)
+        if not os.path.exists(output_pg_fn):
+            texts = text_annotations(fn)
+            with open(output_pg_fn, 'w') as f:
+                f.write(texts)
